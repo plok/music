0250f8994e3631c6e799f40cb38742a2ece64681
unknown <Gerwin@.(none)>
2011-01-09 17:02:00 +0100

    Auto format.

--- MusicShare-swing/src/main/java/org/plok/musicshare/App.java
+++ MusicShare-swing/src/main/java/org/plok/musicshare/App.java
@@ -5,61 +5,65 @@ import java.awt.Dimension;
 import javax.ejb.EJB;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
 import org.plok.musicshare.facade.MusicianFacadeRemote;
 
 /**
  * Hello world!
- *
  */
-public class App 
+public class App
 {
-    @EJB
-    private static MusicianFacadeRemote facade;
+	@EJB
+	private static MusicianFacadeRemote facade;
 
-    
-    public void run(){
-        
-        JFrame frame = new JFrame("demo");
-        
-        int count = facade.count();
-        
-        frame.getContentPane().add(new JLabel(String.valueOf(count)),BorderLayout.CENTER);
-        
-                JLabel emptyLabel = new JLabel("");
-        emptyLabel.setPreferredSize(new Dimension(175, 100));
-        frame.getContentPane().add(emptyLabel,BorderLayout.NORTH);
-        
-        frame.pack();
-        frame.setVisible(true);
 
-    }
-    
-    
-        private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("FrameDemo");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public void run()
+	{
 
-        JLabel emptyLabel = new JLabel("");
-        emptyLabel.setPreferredSize(new Dimension(175, 100));
+		JFrame frame = new JFrame("demo");
 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
+		int count = facade.count();
 
-    
-    public static void main( String[] args )
-    {
-     
-                //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                   App app = new App();
-               app.run();
-            }
-        });
-      
-    }
+		frame.getContentPane().add(new JLabel(String.valueOf(count)), BorderLayout.CENTER);
+
+		JLabel emptyLabel = new JLabel("");
+		emptyLabel.setPreferredSize(new Dimension(175, 100));
+		frame.getContentPane().add(emptyLabel, BorderLayout.NORTH);
+
+		frame.pack();
+		frame.setVisible(true);
+
+	}
+
+
+	private static void createAndShowGUI()
+	{
+		//Create and set up the window.
+		JFrame frame = new JFrame("FrameDemo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel emptyLabel = new JLabel("");
+		emptyLabel.setPreferredSize(new Dimension(175, 100));
+
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+
+	public static void main(String[] args)
+	{
+
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				App app = new App();
+				app.run();
+			}
+		});
+
+	}
 }
