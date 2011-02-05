02affc7b0beae0a025f6194fd41f38020769c413
unknown <Gerwin@.(none)>
2011-02-05 12:43:04 +0100

    whitespace.

--- speeltuin/src/test/java/org/plok/musicshare/model/scale/MelodicMinorScaleTest.java
+++ speeltuin/src/test/java/org/plok/musicshare/model/scale/MelodicMinorScaleTest.java
@@ -18,28 +18,28 @@ public class MelodicMinorScaleTest extends IntervalledScaleTest
 		 */
 		final TestCase[] testCases = new TestCase[]
 		{
-			new TestCase( new MelodicMinorScale( Pitch.C_NATURAL ), "C - D - D# - F - G - A - B - C"    ),
+			new TestCase( new MelodicMinorScale( Pitch.C_NATURAL ), "C - D - D# - F - G - A - B - C"      ),
 			new TestCase( new MelodicMinorScale( Pitch.C_SHARP   ), "C# - D# - E - F# - G# - A# - C - C#" ),
 
 			new TestCase( new MelodicMinorScale( Pitch.D_FLAT    ), "Db - D# - E - F# - G# - A# - C - Db" ),
-			new TestCase( new MelodicMinorScale( Pitch.D_NATURAL ), "D - E - F - G - A - B - C# - D"    ),
-			new TestCase( new MelodicMinorScale( Pitch.D_SHARP   ), "D# - F - F# - G# - A# - C - D - D#" ),
+			new TestCase( new MelodicMinorScale( Pitch.D_NATURAL ), "D - E - F - G - A - B - C# - D"      ),
+			new TestCase( new MelodicMinorScale( Pitch.D_SHARP   ), "D# - F - F# - G# - A# - C - D - D#"  ),
 
-			new TestCase( new MelodicMinorScale( Pitch.E_FLAT    ), "Eb - F - F# - G# - A# - C - D - Eb" ),
+			new TestCase( new MelodicMinorScale( Pitch.E_FLAT    ), "Eb - F - F# - G# - A# - C - D - Eb"  ),
 			new TestCase( new MelodicMinorScale( Pitch.E_NATURAL ), "E - F# - G - A - B - C# - D# - E"    ),
 
-			new TestCase( new MelodicMinorScale( Pitch.F_NATURAL ), "F - G - G# - A# - C - D - E - F"   ),
+			new TestCase( new MelodicMinorScale( Pitch.F_NATURAL ), "F - G - G# - A# - C - D - E - F"     ),
 			new TestCase( new MelodicMinorScale( Pitch.F_SHARP   ), "F# - G# - A - B - C# - D# - F - F#"  ),
 
 			new TestCase( new MelodicMinorScale( Pitch.G_FLAT    ), "Gb - G# - A - B - C# - D# - F - Gb"  ),
-			new TestCase( new MelodicMinorScale( Pitch.G_NATURAL ), "G - A - A# - C - D - E - F# - G"   ),
-			new TestCase( new MelodicMinorScale( Pitch.G_SHARP   ), "G# - A# - B - C# - D# - F - G - G#" ),
+			new TestCase( new MelodicMinorScale( Pitch.G_NATURAL ), "G - A - A# - C - D - E - F# - G"     ),
+			new TestCase( new MelodicMinorScale( Pitch.G_SHARP   ), "G# - A# - B - C# - D# - F - G - G#"  ),
 
-			new TestCase( new MelodicMinorScale( Pitch.A_FLAT    ), "Ab - A# - B - C# - D# - F - G - Ab" ),
+			new TestCase( new MelodicMinorScale( Pitch.A_FLAT    ), "Ab - A# - B - C# - D# - F - G - Ab"  ),
 			new TestCase( new MelodicMinorScale( Pitch.A_NATURAL ), "A - B - C - D - E - F# - G# - A"     ),
-			new TestCase( new MelodicMinorScale( Pitch.A_SHARP   ), "A# - C - C# - D# - F - G - A - A#" ),
+			new TestCase( new MelodicMinorScale( Pitch.A_SHARP   ), "A# - C - C# - D# - F - G - A - A#"   ),
 
-			new TestCase( new MelodicMinorScale( Pitch.B_FLAT    ), "Bb - C - C# - D# - F - G - A - Bb" ),
+			new TestCase( new MelodicMinorScale( Pitch.B_FLAT    ), "Bb - C - C# - D# - F - G - A - Bb"   ),
 			new TestCase( new MelodicMinorScale( Pitch.B_NATURAL ), "B - C# - D - E - F# - G# - A# - B"   ),
 		};
 
